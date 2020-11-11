82edc28615a8a3ef384278c8160e675dce276029
jekshenov93@mail.ru <jekshenov93@mail.ru>
2020-11-11 16:10:19 +0300

    commit

---
+++ src/main/java/pkg/Main.java
@@ -0,0 +1,10 @@
+package pkg;
+
+/**
+ * Created by 99670 on 11.11.2020.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("aloha salam");
+    }
+}
