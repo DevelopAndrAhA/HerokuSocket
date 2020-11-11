978a5cdc88b49f7773a7e6427fa15b80d3388b98
jekshenov93@mail.ru <jekshenov93@mail.ru>
2020-11-11 17:33:07 +0300

    commmitttttttttttt

--- src/main/java/pkg/aloha/Main.java
+++ src/main/java/pkg/aloha/Main.java
@@ -17,10 +17,13 @@ public class Main {
             Socket socket = serverSocket.accept();
             InetAddress inetAddress =  socket.getInetAddress();
             System.out.println(inetAddress.toString()+" это адрес клиента");
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            System.out.println( bufferedReader.readLine());
+            /*BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            System.out.println( bufferedReader.readLine());*/
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            bufferedWriter.write("aloha salam s clienta");
+            bufferedWriter.close();
            /* Socket socket = new Socket();
-            socket.connect(new InetSocketAddress("www.socketkg.herokuapp.com",80));
+            socket.connect(new InetSocketAddress("socketkg.herokuapp.com",80));
 
             BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             bufferedWriter.write("aloha salam s clienta");*/
